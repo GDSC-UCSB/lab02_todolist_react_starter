d87ed9894060f369549d5bf2353a412aa7f86f94
Rachel <racheljiang310@gmail.com>
2022-10-11 21:21:02 -0700

    final touches

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-//DON'T TOUCH THIS FILE
-import './App.css';
-import TodoList from './components/TodoList';
-//Source Credits to: https://www.youtube.com/watch?v=E1E08i2UJGI&ab_channel=BrianDesign
+// TODO: import the right files and add the component to your return statement in App()
 
-/* App.js will be the only js file called my index.js
-*/
+import './App.css';
+
+// import TodoList HERE
+
+
+//App.js will be the only js file called my index.js
+
 function App() {
   return (
+    
+  //TODO: replace 'So Empty' with your TodoList component so we can see it 
+  // when we run npm start
+  
     <div className="todo-app">
-      <TodoList/>
+      So Empty
     </div>
+
   );
 }
-
 export default App;
+
+// Lab 02: React
+//Source Credits to: https://www.youtube.com/watch?v=E1E08i2UJGI&ab_channel=BrianDesign
